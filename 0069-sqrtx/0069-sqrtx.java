794d4663b237521aa1157ce7d60553a0f639ff73
nadeem mallick <nadeemmallick069@gmail.com>
2026-06-30 17:31:29 +0530

    Time: 1 ms (97.73%), Space: 42.5 MB (61.13%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,14 +1,36 @@
 class Solution {
     public int mySqrt(int x) {
-        int ans = -1;
-        if (x == 0) {
-            return 0;
-        }
 
-        for (long i = 1; i * i <= x; i++) {
-            ans = (int) i;
+        //Brute force approch->
+        //         int ans = -1;
+        //         if (x == 0) {
+        //             return 0;
+        //         }
+
+        //         for (long i = 1; i * i <= x; i++) {
+        //             ans = (int) i;
+        //         }
+        //         return ans;
+
+        //opyimal approch
+
+        int ans = -1;
+        int s = 0;
+        int e = x;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            long sq = (long) mid * mid;
+
+            if (sq == x) {
+                return mid;
+            } else if (sq > x) {
+                e = mid - 1;
+            } else {
+                ans = mid;
+                s = mid + 1;
+            }
         }
         return ans;
-
     }
 }
