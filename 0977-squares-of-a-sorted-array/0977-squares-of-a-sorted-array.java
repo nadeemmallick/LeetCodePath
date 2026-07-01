67602c3a74db4885d6e10b2b3f5d6ad1bfa01358
nadeem mallick <nadeemmallick069@gmail.com>
2026-07-02 00:59:10 +0530

    Time: 1 ms (100%), Space: 47 MB (94.12%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,28 +1,48 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
 
-    int n = nums.length;
-    int[] ans = new int[n];
+        //         int n = nums.length;
+        //         int[] ans = new int[n];
 
-    // Step 1: Square every element
-    for (int i = 0; i < n; i++) {
-        ans[i] = nums[i] * nums[i];
-    }
+        //         // Step 1: Square every element
+        //         for (int i = 0; i < n; i++) {
+        //             ans[i] = nums[i] * nums[i];
+        //         }
 
-    // Step 2: Bubble Sort
-    for (int i = 0; i < n - 1; i++) {
+        //         // Step 2: Bubble Sort
+        //         for (int i = 0; i < n - 1; i++) {
 
-        for (int j = 0; j < n - i - 1; j++) {
+        //             for (int j = 0; j < n - i - 1; j++) {
 
-            if (ans[j] > ans[j + 1]) {
+        //                 if (ans[j] > ans[j + 1]) {
 
-                int temp = ans[j];
-                ans[j] = ans[j + 1];
-                ans[j + 1] = temp;
+        //                     int temp = ans[j];
+        //                     ans[j] = ans[j + 1];
+        //                     ans[j + 1] = temp;
+        //                 }
+        //             }
+        //         }
+
+        //         return ans;
+        int n = nums.length;
+        int ans[] = new int[n];
+        int left = 0;
+        int right = n-1;
+        int k = n-1;
+
+        while(left <= right){
+            int leftSquare = nums[left]*nums[left];
+            int rightSquare = nums[right]*nums[right];
+
+            if(leftSquare > rightSquare){
+                ans[k] = leftSquare;
+                left++;
+            }else{
+                ans[k] = rightSquare;
+                right--;
             }
+            k--;
         }
+        return ans;
     }
-
-    return ans;
 }
- }
