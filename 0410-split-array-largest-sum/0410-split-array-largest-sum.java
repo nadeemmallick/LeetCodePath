f337647d59ce6de9c3a044a0cfb211eff2f8a374
nadeem mallick <nadeemmallick069@gmail.com>
2026-07-02 15:26:19 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.93%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,55 +1,48 @@
 class Solution {
-    public static boolean isValid(int[] nums, int k, int mid) {
-        int Kcount = 1; // we take first count in start
-        int numberSum = 0; //and take the number sum
 
-        for (int i = 0; i < nums.length; i++) {
-            //in first if condition we check is the numbersum + nums[i] is less then mid if yes then we add the num i in number sum
-            if (numberSum + nums[i] <= mid) {
-                numberSum = numberSum + nums[i];
-            } else {
-                //if the first condition is not fullfill then we come in else loop to return true or sum of next student
-                Kcount++;
-                if (Kcount > k || nums[i] > mid) { // return false if Kcount is greater than the k or nums i greater than mid
+    public static boolean Isvalid(int[] nums, int k, int mid){
+        int KCount = 1;
+        int numbersum = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            if(numbersum + nums[i] <= mid){
+                numbersum = numbersum + nums[i];
+            }else{
+                KCount++;
+                if(KCount > k || nums[i] > mid){
                     return false;
-                } else { // if not we simply perform else numbersum 
-                    numberSum = 0;
-                    numberSum = numberSum + nums[i];
+                }else{
+                    numbersum = 0;
+                    numbersum = numbersum+nums[i];
                 }
             }
         }
         return true;
     }
-
     public int splitArray(int[] nums, int k) {
-
-        // solve when the nums length is greater than the give k value
-        if (nums.length < k) {
+        if(nums.length < k){
             return -1;
         }
-
         int n = nums.length;
-        int s = 0;
-        int sum = 0; // we take sum because the answer come between 0 -> numsSum
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum+nums[i];
         }
-        int e = sum;
-        int ans = -1;
+        int s = 0;
+        int end = sum;
+        int ans =-1;
 
-        while (s <= e) {
+        while(s<=end){
+            int mid = s+(end-s)/2;
 
-            int mid = s + (e - s) / 2;
-
-            //we make a proper function to handle valid operation and we pass nums,value of k and mid 
-            if (isValid(nums, k, mid)) {
+            if(Isvalid(nums,k,mid)){
                 ans = mid;
-                e = mid - 1;
-            } else {
-                s = mid + 1;
+                end = mid-1;
+            }else{
+                s= mid+1;
             }
         }
-
         return ans;
+        
     }
 }
