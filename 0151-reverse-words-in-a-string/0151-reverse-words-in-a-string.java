0ea7c085e17e4dbeb05ac27535c98b4b37327fa5
nadeem mallick <nadeemmallick069@gmail.com>
2026-07-11 13:57:12 +0530

    Time: 3 ms (98.96%), Space: 44.1 MB (87.3%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,20 +1,51 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
+        // String[] words = s.trim().split("\\s+");
+
+        // StringBuilder ans = new StringBuilder();
+
+        // for (int i = words.length - 1; i >= 0; i--) {
+
+        //     ans.append(words[i]);
+
+        //     if (i != 0) {
+        //         ans.append(" ");
+        //     }
+        // }
+
+        // return ans.toString();
 
         StringBuilder ans = new StringBuilder();
+        int i = s.length() - 1;
 
-        for (int i = words.length - 1; i >= 0; i--) {
+        while (i >= 0) {
 
-            ans.append(words[i]);
-
-            if (i != 0) {
-                ans.append(" ");
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
             }
-        }
+            if (i < 0) {
+                break;
+            }
 
+            int j = i;
+
+            while (j >= 0 && s.charAt(j) != ' ') {
+                j--;
+            }
+
+            ans.append(s.substring(j + 1, i + 1));
+
+            while (j >= 0 && s.charAt(j) == ' ') {
+                j--;
+            }
+
+            if (j >= 0) {
+                ans.append(' ');
+            }
+
+            i = j;
+
+        }
         return ans.toString();
-        
-        
     }
 }
