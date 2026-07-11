fe316793d6f41db25857021ebd16eaf7ca318ed1
nadeem mallick <nadeemmallick069@gmail.com>
2026-07-11 23:56:30 +0530

    Time: 3 ms (57.69%), Space: 47.3 MB (10.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,49 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
+        // int n = nums.length;
 
-        for(int i = 0; i<n-1; i++){
+        // for(int i = 0; i<n-1; i++){
 
-            for(int j= i+1 ; j<n; j++)
-            if(nums[i] + nums[j] == target){
-                int [] ans = {i ,j};
-                return ans;
+        //     for(int j= i+1 ; j<n; j++)
+        //     if(nums[i] + nums[j] == target){
+        //         int [] ans = {i ,j};
+        //         return ans;
 
+        //     }
+        // }
+        // int [ ] ans ={};
+        // return ans;
+          HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+
+            map.put(nums[i], i);
         }
-        int [ ] ans ={};
-        return ans;
-        
+
+        return new int[]{-1, -1};
+        // int n = nums.length;
+        // int Left = 0;
+        // int Right = n-1;
+        //  Arrays.sort(nums);
+
+        // while(Left < Right){
+        //     int sum = nums[Left] + nums[Right];
+
+        //     if(sum == target){
+        //          return new int[]{Left,Right};
+        //     }else if(sum < target){
+        //         Left++;
+        //     }else{
+        //         Right--;
+        //     }
+        // }
+        // int [ ] ans ={};
+        //  return ans;
     }
 }
