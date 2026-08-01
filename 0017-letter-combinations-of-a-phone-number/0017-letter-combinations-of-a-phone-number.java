b1f88fdd467f8f322955869aa0ee52348ae87dae
nadeem mallick <nadeemmallick069@gmail.com>
2026-08-01 20:45:03 +0530

    Time: 0 ms (100%), Space: 43.6 MB (38.18%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String[] mapping = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        int index = 0;
+        List<String> result = new ArrayList<>();
+        StringBuilder output = new StringBuilder();
+        solve(digits, mapping, index, result, output);
+        return result;
+
+    }
+
+    public static void solve(String digits,String[] mapping, int index, List<String> result,StringBuilder output)
+    {
+        //base case
+        if(index >= digits.length()){
+            result.add(output.toString());
+            return;
+        }
+
+        int value = digits.charAt(index) - '0';
+        String mappedString = mapping[value];
+
+        for(int i =0; i < mappedString.length(); i++){
+            //Recursive call
+            output.append(mappedString.charAt(i));
+            solve(digits,mapping,index+1,result,output);
+        //Backtracking
+        output.deleteCharAt(output.length()-1);
+        }
+    }
+}
