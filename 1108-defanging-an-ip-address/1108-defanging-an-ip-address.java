54aad1d04899eee63c1dfb691dcda92812075ad8
nadeem mallick <nadeemmallick069@gmail.com>
2026-08-12 11:27:59 +0530

    Time: 4 ms (2.67%), Space: 43.1 MB (11.77%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String result = "";
+
+        for (int i = 0; i < address.length(); i++) {
+            char ch = address.charAt(i);
+
+            if (ch == '.') {
+                result = result + "[.]";
+            } else {
+                result = result + ch;
+            }
+        }
+        return result;
+
+    }
+}
