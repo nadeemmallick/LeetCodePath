da515cd59774ad31b18ccf6598195b63f11e9368
nadeem mallick <nadeemmallick069@gmail.com>
2026-08-20 16:19:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (78.07%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,17 +1,19 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String answer = "";
+    //     String answer = "";
        
-       for(int i= 0; i<address.length(); i++){
-        char ch = address.charAt(i);
+    //    for(int i= 0; i<address.length(); i++){
+    //     char ch = address.charAt(i);
 
-        if(ch == '.'){
-            answer = answer+ "[.]";
-        }else{
-            answer= answer+ch;
-        }
-       }
-       return answer;
+    //     if(ch == '.'){
+    //         answer = answer+ "[.]";
+    //     }else{
+    //         answer= answer+ch;
+    //     }
+    //    }
+    //    return answer;
+    return address.replace(".","[.]");
+    
         
         
     }
