28efcfa6bf7e425e968b54ce94fdf9bf6e87914d
nadeem mallick <nadeemmallick069@gmail.com>
2026-09-05 01:09:04 +0530

    Time: 0 ms (100%), Space: 46.2 MB (75.21%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,54 +10,77 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // if (head == null) {
+        //     return head;
+        // }
+
+        // ListNode oddH = null;
+        // ListNode oddT = null;
+
+        // ListNode evenH = null;
+        // ListNode evenT = null;
+
+        // ListNode curr = head;
+        // int position = 1;
+
+        // while (curr != null) {
+
+        //     ListNode newNode = new ListNode(curr.val);
+
+        //     if (position % 2 == 1) {
+
+        //         // Odd position
+        //         if (oddH == null) {
+        //             oddH = newNode;
+        //             oddT = newNode;
+        //         } else {
+        //             oddT.next = newNode;
+        //             oddT = newNode;
+        //         }
+
+        //     } else {
+
+        //         // Even position
+        //         if (evenH == null) {
+        //             evenH = newNode;
+        //             evenT = newNode;
+        //         } else {
+        //             evenT.next = newNode;
+        //             evenT = newNode;
+        //         }
+        //     }
+
+        //     position++;
+        //     curr = curr.next;
+        // }
+
+        // // Odd list + Even list
+        // oddT.next = evenH;
+
+        // return oddH;
         if (head == null) {
             return head;
         }
-
-        ListNode oddH = null;
-        ListNode oddT = null;
-
-        ListNode evenH = null;
-        ListNode evenT = null;
-
-        ListNode curr = head;
-        int position = 1;
-
-        while (curr != null) {
-
-            ListNode newNode = new ListNode(curr.val);
-
-            if (position % 2 == 1) {
-
-                // Odd position
-                if (oddH == null) {
-                    oddH = newNode;
-                    oddT = newNode;
-                } else {
-                    oddT.next = newNode;
-                    oddT = newNode;
-                }
-
-            } else {
-
-                // Even position
-                if (evenH == null) {
-                    evenH = newNode;
-                    evenT = newNode;
-                } else {
-                    evenT.next = newNode;
-                    evenT = newNode;
-                }
-            }
-
-            position++;
-            curr = curr.next;
+        if (head.next == null) {
+            return head;
         }
 
-        // Odd list + Even list
-        oddT.next = evenH;
+        ListNode OddHead = head;
+        ListNode OddTail = head;
 
-        return oddH;
+        ListNode EvenHead = head.next;
+        ListNode EvenTail = head.next;
+
+        while (EvenTail != null && EvenTail.next != null) {
+            OddTail.next = EvenTail.next;
+            OddTail = EvenTail.next;
+
+            EvenTail.next = OddTail.next;
+            EvenTail = OddTail.next;
+
+        }
+
+        OddTail.next = EvenHead;
+        return OddHead;
     }
-
 }
