b0c943d53a2ee2fec45a2f271451b081de5c1a37
nadeem mallick <nadeemmallick069@gmail.com>
2026-09-06 20:10:48 +0530

    Time: 4 ms (100%), Space: 225.7 MB (70.79%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -11,29 +11,45 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
 
-        ListNode ansH = null;
-        ListNode ansT = null;
+        // ListNode ansH = null;
+        // ListNode ansT = null;
 
-        ListNode temp = head.next;
-        int sum = 0;
+        // ListNode temp = head.next;
+        // int sum = 0;
 
-        while (temp != null) {
-            if (temp.val != 0) {
-                sum = sum + temp.val;
-            } else {
-                ListNode newNode = new ListNode(sum);
+        // while (temp != null) {
+        //     if (temp.val != 0) {
+        //         sum = sum + temp.val;
+        //     } else {
+        //         ListNode newNode = new ListNode(sum);
 
-                if (ansH == null) {
-                    ansH = newNode;
-                    ansT = newNode;
-                } else {
-                    ansT.next = newNode;
-                    ansT = newNode;
-                }
-                sum = 0;
+        //         if (ansH == null) {
+        //             ansH = newNode;
+        //             ansT = newNode;
+        //         } else {
+        //             ansT.next = newNode;
+        //             ansT = newNode;
+        //         }
+        //         sum = 0;
+        //     }
+        //     temp = temp.next;
+        // }
+        // return ansH;
+
+        ListNode read = head.next;
+        ListNode write = head;
+
+        while (read != null) {
+            int sum = 0;
+            while (read.val != 0) {
+                sum = sum + read.val;
+                read = read.next;
             }
-            temp = temp.next;
+            write.val = sum;
+            write.next = read.next;
+            read = read.next;
+            write = write.next;
         }
-        return ansH;
+        return head;
     }
 }
