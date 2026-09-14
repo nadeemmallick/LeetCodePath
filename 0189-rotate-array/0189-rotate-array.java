67aa2318bb2cf3be56ab453bff1ade43f303d60e
nadeem mallick <nadeemmallick069@gmail.com>
2026-09-14 19:13:23 +0530

    Time: 5 ms (24.01%), Space: 268.5 MB (38.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+
         int n = nums.length;
-        k = k%n;
 
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
+        k = k % n; // k ke small value nikal le 
 
+        reverse(nums, 0, n - 1); // phle hum pure array ko reverse krr dete hai
+        reverse(nums, 0, k - 1);//uske badd  array ke first half ko rotate kare gai 
+        reverse(nums, k, n - 1);//then baki bacche hua part ko rotate krr de gai
 
-        
     }
-    public static void reverse(int[] nums, int start, int end){
 
-        while(start < end){
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
