c3bfc8040d9cb2b57774873d5eb0d1d31ec59c04
nadeem mallick <nadeemmallick069@gmail.com>
2026-09-20 21:00:38 +0530

    Time: 0 ms (100%), Space: 46.6 MB (16.84%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,42 +10,71 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-
         int len = 0;
         ListNode temp = head;
+
         while (temp != null) {
             len++;
             temp = temp.next;
         }
 
-        int group = len / k;
-
-        ListNode curr = head;
-        ListNode ansH = null;
-        ListNode ansT = null;
-
-        while (group > 0) {
-
-            ListNode GH = curr;
-            ListNode prev = null;
-
-            for (int i = 0; i < k; i++) {
-                ListNode forward = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = forward;
-            }
-
-            if (ansH == null) {
-                ansH = prev;
-            } else {
-                ansT.next = prev;
-            }
-            ansT = GH;
-            group--;
+        if (len < k) {
+            return head;
         }
-        ansT.next = curr;
-        return ansH;
 
+        ListNode prev = null;
+        ListNode curr = head;
+
+        for (int i = 1; i <= k; i++) {
+
+            ListNode forward = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = forward;
+        }
+
+        ListNode recursionAnsHead = reverseKGroup(curr, k);
+
+        head.next = recursionAnsHead;
+
+        return prev;
+        //ITERATIVE METHOD
+        // int len = 0;
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     len++;
+        //     temp = temp.next;
+        // }
+
+        // int group = len / k;
+
+        // ListNode curr = head;
+        // ListNode ansH = null;
+        // ListNode ansT = null;
+
+        // while (group > 0) {
+
+        //     ListNode GH = curr;
+        //     ListNode prev = null;
+
+        //     for (int i = 0; i < k; i++) {
+        //         ListNode forward = curr.next;
+        //         curr.next = prev;
+        //         prev = curr;
+        //         curr = forward;
+        //     }
+
+        //     if (ansH == null) {
+        //         ansH = prev;
+        //     } else {
+        //         ansT.next = prev;
+        //     }
+        //     ansT = GH;
+        //     group--;
+        // }
+        // ansT.next = curr;
+        // return ansH
     }
 }
